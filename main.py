3146139a748ce3ec3657c67851eedc32e9fe440f
Chris S <christopher.sutedja@gmail.com>
2023-05-25 19:40:18 -0400

    Update main.py

--- main.py
+++ main.py
@@ -5,18 +5,26 @@ question = str(input("To calculate a mo3, type in 1. To calculate an ao5, type i
 if question == str(1):
   sum_of_times = 0
   for i in range(0,3):
-    time_input = float(input("Please input a time, seperating each one with ENTER key "))
-    time_list.append(time_input)
+    try:
+      time_input = float(input("Please input a time in seconds, seperating each one with ENTER key "))
+      time_list.append(time_input)
+    except ValueError:
+      print("Invalid input! ")
+      raise SystemExit
   sum_of_times = sum([float(x) for x in time_list])
   mean_of_three = sum_of_times / 3
-  print("Your mean of 3 is " + str(round(mean_of_three, 2)) + " seconds.")
+  print("\n" + "Your mean of 3 is " + str(round(mean_of_three, 2)) + " seconds.")
 
 elif question == str(2):
  sum_of_times = 0
  for i in range(0, 5):
-  time_input = float(input("Please input a time, seperating each one with ENTER key "))
-  time_list.append(time_input)
-  sum_of_times = sum([float(x) for x in time_list])
+    try:
+      time_input = float(input("Please input a time in seconds, seperating each one with ENTER key "))
+      time_list.append(time_input)
+    except ValueError:
+      print("Invalid input! ")
+      raise SystemExit
+ sum_of_times = sum([float(x) for x in time_list])  
  maxnum = 0
  minnum = 0
  def find_min_num(list): # function to determine fastest time 
@@ -37,12 +45,16 @@ elif question == str(2):
  maxnum = find_max_num(time_list)
  three_solves = float(sum_of_times) - float(minnum) - float(maxnum) 
  ao5 = three_solves/3
- print("Your average of 5 is " + str(round(ao5, 2)) + " seconds.")
+ print("\n" + "Your average of 5 is " + str(round(ao5, 2)) + " seconds.")
 
 elif question == str(3):
   for i in range(0, 12):
-    time_input = float(input("Please input a time, seperating each one with ENTER key "))
-    time_list.append(time_input)
+    try:
+      time_input = float(input("Please input a time in seconds, seperating each one with ENTER key "))
+      time_list.append(time_input)
+    except ValueError:
+      print("Invalid input! ") 
+      raise SystemExit 
   sum_of_times = sum([float(x) for x in time_list])
   maxnum = 0
   minnum = 0
@@ -63,7 +75,7 @@ elif question == str(3):
   maxnum = find_max_num(time_list)
   three_solves = float(sum_of_times) - float(minnum) - float(maxnum) 
   ao12 = three_solves/10
-  print("Your average of 12 is " + str(round(ao12, 2)) + " seconds.")
+  print("\n" + "Your average of 12 is " + str(round(ao12, 2)) + " seconds.")
 
 else:
   print("Invalid input! ")
